1d40a083afebc6f6a4d11ac6cb9a142bf6761208
VINCENZO_JOVANOVIC <2151301@talnet.nl>
2021-12-27 23:24:47 +0100

    taak01 af en een afbeelding erbij omdat ik de code vergeten ben te plakken

--- 1-INSERT/taak01 - Insert Into/antwoorden.sql
+++ 1-INSERT/taak01 - Insert Into/antwoorden.sql
@@ -1,1 +1,7 @@
+INSERT INTO studenten (`student_id`, `voornaam`, `achternaam`, `klas`, `adres`, `postcode`, `geboortedatum`, `woonplaats`) 
+VALUES (1, 'henk', 'van Balen', '9A', 'Albert Cuypstraat 1', '1234AB', '2000-05-18', 'Amsterdam');
 
+INSERT INTO studenten (`student_id`, `voornaam`, `achternaam`, `klas`, `adres`, `postcode`, `geboortedatum`, `woonplaats`) 
+VALUES (5, 'Piet', 'Pieters', '9A', 'Albert Cuypstraat 5', '1234AB', '2001-04-28', 'Ouderkerk');
+
+/*ik was vergeten om de overige code te plakken dus ik heb ook een afbeelding erbij gezet ik heb die ook gepusht*/
