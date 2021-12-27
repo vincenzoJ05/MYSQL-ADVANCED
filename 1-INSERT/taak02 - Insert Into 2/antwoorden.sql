13f0386ded8bb85207d2f7b5f02372d7e7d9d48c
VINCENZO_JOVANOVIC <2151301@talnet.nl>
2021-12-27 23:41:08 +0100

    taak02 af

--- 1-INSERT/taak02 - Insert Into 2/antwoorden.sql
+++ 1-INSERT/taak02 - Insert Into 2/antwoorden.sql
@@ -1,1 +1,8 @@
+INSERT INTO studenten (student_id, voornaam , achternaam, klas, geboortedatum, woonplaats) 
+VALUES (8, 'Jan' , 'Vermeer' , '9C', '2000-08-08', 'Amsterdam')
 
+INSERT INTO studenten (student_id, voornaam , achternaam, klas, geboortedatum, woonplaats)
+VALUES (9, 'Bert' , 'Hendriks' , '9C', '2001-04-28', 'Utrecht')
+
+INSERT INTO studenten (student_id, voornaam , achternaam, klas, geboortedatum, woonplaats) 
+VALUES (10, 'Marri' , 'Janssen' , '9C', '2003-06-26', 'Almere')
