161feddc13f024c9b3a7d3b6b4251437bfbca418
VINCENZO_JOVANOVIC <2151301@talnet.nl>
2021-12-28 22:01:14 +0100

    taak02 af

--- 2-UPDATE/taak02 - Update 2/antwoorden.sql
+++ 2-UPDATE/taak02 - Update 2/antwoorden.sql
@@ -1,8 +1,8 @@
 -- Opdracht 1
-
+UPDATE studenten SET klas = '9X' WHERE klas = '9A'
 -- Opdracht 2 
-
+UPDATE studenten SET klas = '9Z' WHERE student_id > '7'
 -- Opdracht 3
-
+UPDATE studenten SET woonplaats = 'Amsterdam' WHERE student_id <= 2 OR student_id > 7
 -- Opdracht 4
-
+UPDATE studenten SET woonplaats = 'Uithoorn' WHERE klas = '9X' AND woonplaats = 'Amsterdam'
