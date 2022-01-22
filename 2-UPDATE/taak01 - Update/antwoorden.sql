5d2449f353bd69424a14c78557b83914d845deb1
VINCENZO_JOVANOVIC <2151301@talnet.nl>
2022-01-22 19:29:59 +0100

    verbeterd

--- 2-UPDATE/taak01 - Update/antwoorden.sql
+++ 2-UPDATE/taak01 - Update/antwoorden.sql
@@ -1,9 +1,31 @@
 -- Opdracht 1
-
+UPDATE studenten 
+SET woonplaats = 'Amstelveen'
 -- Opdracht 2 
-
+UPDATE studenten 
+SET adres = 'jan van galenstraat 2'
+WHERE student_id = 8
+/* tweede statement zelfde opdracht */
+UPDATE studenten 
+SET adres = 'Ferdinand Bolstraat 6'
+WHERE student_id = 9
+/* derde statement zelfde opdracht */
+UPDATE studenten 
+SET adres = 'van boshuizenstraat 8'
+WHERE student_id = 10
 -- Opdracht 3
-
+UPDATE studenten
+SET geboortedatum = 2000-12-12
+WHERE student_id = 2
 -- Opdracht 4
-
+UPDATE studenten
+SET klas = 9C
+WHERE student_id = 4
+/* tweede statement zelfde opdr*/
+UPDATE studenten
+SET klas = 9A
+WHERE student_id = 8
 -- Opdracht 5
+UPDATE studenten
+SET voornaam = Johannah
+WHERE student_id = 6 
